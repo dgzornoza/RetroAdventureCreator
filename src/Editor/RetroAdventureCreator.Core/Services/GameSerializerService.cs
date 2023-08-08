31dc8559d72f9539d926b35300e62f2f0558a577
David González Zornoza <dgzornoza@dgzornoza.com>
2023-08-08 12:53:19 +0200

    wip

--- src/Editor/RetroAdventureCreator.Core/Services/GameSerializerService.cs
+++ src/Editor/RetroAdventureCreator.Core/Services/GameSerializerService.cs
@@ -20,6 +20,20 @@ internal class GameSerializerService
     {
         var serializerBuilder = new SerializerFactory(gameModel);
 
+        var commandGroupSerializer = serializerBuilder.GetSerializer<CommandGroupSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var commandsSerializer = serializerBuilder.GetSerializer<CommandsSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var afterInputCommandDispatchersSerializer = serializerBuilder.GetSerializer<AfterInputCommandDispatchersSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var beforeInputCommandDispatchersSerializer = serializerBuilder.GetSerializer<BeforeInputCommandDispatchersSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var flagsSerializer = serializerBuilder.GetSerializer<FlagsSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var inputCommandsSerializer = serializerBuilder.GetSerializer<InputCommandsSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var messagesSerializer = serializerBuilder.GetSerializer<MessagesSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var objectsSerializer = serializerBuilder.GetSerializer<ObjectsSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var playerSerializer = serializerBuilder.GetSerializer<PlayerSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var scenesSerializer = serializerBuilder.GetSerializer<ScenesSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var settingsSerializer = serializerBuilder.GetSerializer<SettingsSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var vocabularyNounsSerializer = serializerBuilder.GetSerializer<VocabularyNounsSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+        var vocabularyVerbsSerializer = serializerBuilder.GetSerializer<VocabularyVerbsSerializer>().Serialize(serializerBuilder.GameComponentsPointersModel);
+
         return null;
     }
 }
---
+++ src/Editor/RetroAdventureCreator.Test/Facts/ServicesTests/GameSerializerServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RetroAdventureCreator.Core.Services;
+using RetroAdventureCreator.Test.Infrastructure.Builders;
+
+namespace RetroAdventureCreator.Test.Facts.ServicesTests;
+
+public class GameSerializerServiceTest
+{
+    [Fact]
+    public void FlagsSerializerTest_Serialize_AsExpected()
+    {
+        // Arrange
+        var builder = new GameInPawsTutorialBuilder();
+        var game = builder.BuildGame();
+
+        // Act
+        var service = new GameSerializerService().Serialize(game);
+
+        // Assert
+    }
+}
